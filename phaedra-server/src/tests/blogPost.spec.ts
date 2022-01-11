5c264051300c5596a43a0f69b78918d6dd99f080
Matt Fung <official.matthiasfung@gmail.com>
2022-01-11 02:49:10 -0800

    12: Add test for GET request

--- phaedra-server/src/tests/blogPost.spec.ts
+++ phaedra-server/src/tests/blogPost.spec.ts
@@ -156,7 +156,71 @@ describe('GET /api/v1/blog-post/:id', () => {
   });
 });
 
-// Test 4: Add a published blog post
+// Test 4: Get an admin blog post by id
+describe('GET /api/v1/admin-blog-post/:id', () => {
+  before((done) => {
+    testApp = setupApp();
+    httpServer = setupHttpServer(testApp);
+    agent = chai.request.agent(testApp);
+    attemptAuthentication(agent, done, Accounts.ADMIN);
+  });
+  after(() => {
+    httpServer.close();
+  });
+  it('should return error code 400 for admin blog post id that is negative or NaN', (done) => {
+    agent.get('/api/v1/admin-blog-post/-1').end((err: any, res: any) => {
+      expect(err).to.be.null;
+      expect(res).to.have.status(400);
+      expect(res.text).to.deep.equal(JSON.stringify(blogPostAdminNegativeOrNanInputError));
+      done();
+    });
+  });
+  it('should return error code 400 for invalid URL', (done) => {
+    agent.get('/api/v1/admin-blog-post/wow').end((err: any, res: any) => {
+      expect(err).to.be.null;
+      expect(res).to.have.status(400);
+      expect(res.text).to.deep.equal(JSON.stringify(blogPostAdminNegativeOrNanInputError));
+      done();
+    });
+  });
+  it('should return error code 404 for admin blog post yet to be created', (done) => {
+    agent.get('/api/v1/admin-blog-post/55').end((err: any, res: any) => {
+      expect(err).to.be.null;
+      expect(res).to.have.status(404);
+      expect(res.text).to.deep.equal(JSON.stringify(blogPostDNEError));
+      done();
+    });
+  });
+  it('should get an admin blog post by id successfully', (done) => {
+    agent.get('/api/v1/admin-blog-post/1').end((err: any, res: any) => {
+      expect(err).to.be.null;
+      expect(res).to.have.status(200);
+      expect(res.body).to.be.an('object');
+      expect(res.body).to.have.deep.property('id');
+      expect(res.body).to.have.deep.property('title');
+      expect(res.body).to.have.deep.property('author');
+      expect(res.body).to.have.deep.property('updated_at');
+      expect(res.body).to.have.deep.property('image_caption');
+      expect(res.body).to.have.deep.property('preview');
+      expect(res.body).to.have.deep.property('content');
+      expect(res.body).to.have.deep.property('is_draft');
+      expect(res.body).to.have.deep.property('image_id');
+      expect(res.body).to.have.deep.property('user_id');
+      expect(res.body.id).to.deep.equal(1);
+      expect(res.body.title).to.deep.equal(titles[0]);
+      expect(res.body.author).to.deep.equal(authors[0]);
+      expect(res.body.image_caption).to.deep.equal(image_captions[0]);
+      expect(res.body.preview).to.deep.equal(previews[0]);
+      expect(res.body.content).to.deep.equal('');
+      expect(res.body.is_draft).to.deep.equal(0);
+      expect(res.body.image_id).to.deep.equal(1);
+      expect(res.body.user_id).to.deep.equal(user_ids[0]);
+      done();
+    });
+  });
+});
+
+// Test 5: Add a published blog post
 describe('POST /api/v1/admin-blog-post/publish', () => {
   before((done) => {
     testApp = setupApp();
@@ -228,7 +292,7 @@ describe('POST /api/v1/admin-blog-post/publish', () => {
   });
 });
 
-// Test 5: Add a draft blog post
+// Test 6: Add a draft blog post
 describe('POST /api/v1/admin-blog-post/draft', () => {
   before((done) => {
     testApp = setupApp();
@@ -300,9 +364,9 @@ describe('POST /api/v1/admin-blog-post/draft', () => {
   });
 });
 
-// Test 6: Edit a blog post by id
+// Test 7: Edit a blog post by id
 
-// Test 7: Remove a blog post by id
+// Test 8: Remove a blog post by id
 describe('DELETE /api/v1/admin-blog-post/:id', () => {
   before((done) => {
     testApp = setupApp();
