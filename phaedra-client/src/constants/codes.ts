9a9bc47cd27a6867fefa41f6819984d44a9ac3ef
Matt Fung <official.matthiasfung@gmail.com>
2022-05-02 20:02:35 -0700

    Add enum support for months and add escape characters parser

--- phaedra-client/src/constants/codes.ts
+++ phaedra-client/src/constants/codes.ts
@@ -1,4 +1,6 @@
 const ERROR_CODE: number = -1;
 const DEFAULT_IMAGE_ID: number = 1;
+const LANGUAGE: string = 'en-US';
+const TIMEZONE: string = 'America/Vancouver';
 
-export { ERROR_CODE, DEFAULT_IMAGE_ID };
+export { ERROR_CODE, DEFAULT_IMAGE_ID, LANGUAGE, TIMEZONE };
